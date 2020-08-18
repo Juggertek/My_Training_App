9ab95e05f66d577bf1054be52247e9a3d029843c
Marcel Fuchs <68725691+Juggertek@users.noreply.github.com>
2020-08-18 17:42:30 +0200

    Meine Trainingsapp zum schrittweisen erlernen von verschiedenen Programmiertechniken. Dies ist die Grundversion der App. Eine einfache Liste mit Darstellung in einer RecyclerView. Die Items können per Button in die Liste eingefügt werden.
    Die App funktioniert soweit, es wird nur die RecyclerView nach Änderung der Liste nicht upgedated.

--- app/src/main/java/com/marcelfuchs/mytrainingapp/MainActivity.kt
+++ app/src/main/java/com/marcelfuchs/mytrainingapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.marcelfuchs.mytrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Liste mit 2 String erstellen
+    val mWordList: ArrayList<String> = ArrayList(listOf<String>("Fenster", "Haus", "Auto"))
+
+    // Variable für neues Wort instanzieren/deklarieren
+    val enteredWord: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvStringList.layoutManager = LinearLayoutManager(this)
+        rvStringList.adapter = RecyclerViewAdapter(mWordList)
+
+        btnAdd.setOnClickListener { addWord() }
+        btnRemove.setOnClickListener { removeWord() }
+
+
+    }
+
+
+    private fun addWord() {
+        mWordList.add(etWord.text.toString())
+    }
+
+    private fun removeWord() {
+        mWordList.remove(etWord.text.toString())
     }
 }
