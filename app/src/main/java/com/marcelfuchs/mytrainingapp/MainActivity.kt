3d2afb94ec3aff67079d16e4aaf4073a88ceaeea
Marcel Fuchs <68725691+Juggertek@users.noreply.github.com>
2020-08-22 10:09:17 -0400

    Mit DiffUtil probiert, nicht geklappt...

--- app/src/main/java/com/marcelfuchs/mytrainingapp/MainActivity.kt
+++ app/src/main/java/com/marcelfuchs/mytrainingapp/MainActivity.kt
@@ -3,13 +3,15 @@ package com.marcelfuchs.mytrainingapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.marcelfuchs.mytrainingapp.databinding.ActivityMainBinding
 
+// Liste mit 2 String erstellen
+var mWordList: ArrayList<String> = ArrayList(listOf("Fenster", "Haus", "Auto"))
+
 class MainActivity : AppCompatActivity() {
 
-    // Liste mit 2 String erstellen
-    private val mWordList: ArrayList<String> = ArrayList(listOf("Fenster", "Haus", "Auto"))
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,14 +25,25 @@ class MainActivity : AppCompatActivity() {
         binding.btnRemove.setOnClickListener { removeWord() }
     }
 
-
     private fun addWord() {
         mWordList.add(binding.etWord.text.toString())
-        binding.rvStringList.adapter?.notifyDataSetChanged()
+//        binding.rvStringList.adapter?.notifyDataSetChanged()
+        updateList(mWordList)
     }
 
     private fun removeWord() {
         mWordList.remove(binding.etWord.text.toString())
-        binding.rvStringList.adapter?.notifyDataSetChanged()
+//        binding.rvStringList.adapter?.notifyDataSetChanged()
+        updateList(mWordList)
+    }
+
+
+    private fun updateList(newList: ArrayList<String>) {
+        val oldList = mWordList
+        val diffResult: DiffUtil.DiffResult =
+            DiffUtil.calculateDiff(RecyclerViewAdapter.ListItemDiffCallback(oldList, newList))
+        mWordList = newList
+        binding.rvStringList.adapter?.let { diffResult.dispatchUpdatesTo(it) }
     }
 }
+
--- app/src/main/java/com/marcelfuchs/mytrainingapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/marcelfuchs/mytrainingapp/RecyclerViewAdapter.kt
@@ -1,8 +1,10 @@
 package com.marcelfuchs.mytrainingapp
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.marcelfuchs.mytrainingapp.databinding.ListItemBinding
 
@@ -68,5 +70,65 @@ class RecyclerViewAdapter(private val arrayList: ArrayList<String>) :
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.tvItem.text = arrayList[position]
+    }
+
+    class ListItemDiffCallback(
+        private var oldList: ArrayList<String>,
+        private var newList: ArrayList<String>
+    ) :
+        DiffUtil.Callback() {
+        /**
+         * Returns the size of the old list.
+         *
+         * @return The size of the old list.
+         */
+        override fun getOldListSize() = oldList.size
+
+        /**
+         * Returns the size of the new list.
+         *
+         * @return The size of the new list.
+         */
+        override fun getNewListSize() = newList.size
+
+        /**
+         * Called by the DiffUtil to decide whether two object represent the same Item.
+         *
+         *
+         * For example, if your items have unique ids, this method should check their id equality.
+         *
+         * @param oldItemPosition The position of the item in the old list
+         * @param newItemPosition The position of the item in the new list
+         * @return True if the two items represent the same object or false if they are different.
+         */
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            Log.d("RecyclerViewAdapter", "areItemsTheSame called")
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+
+        /**
+         * Called by the DiffUtil when it wants to check whether two items have the same data.
+         * DiffUtil uses this information to detect if the contents of an item has changed.
+         *
+         *
+         * DiffUtil uses this method to check equality instead of [Object.equals]
+         * so that you can change its behavior depending on your UI.
+         * For example, if you are using DiffUtil with a
+         * [RecyclerView.Adapter], you should
+         * return whether the items' visual representations are the same.
+         *
+         *
+         * This method is called only if [.areItemsTheSame] returns
+         * `true` for these items.
+         *
+         * @param oldItemPosition The position of the item in the old list
+         * @param newItemPosition The position of the item in the new list which replaces the
+         * oldItem
+         * @return True if the contents of the items are the same or false if they are different.
+         */
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int):Boolean{
+            Log.d("RecyclerViewAdapter", "areContentsTheSame called")
+            return true
+        }
     }
 }
